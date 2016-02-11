e176a2943fd1cf31711cfb4abb8791429519d878
AnatolyKorablyov <anatoly5432@gmail.com>
2016-02-11 19:56:47 +0300

    ООП

--- lab1/lab1/main.cpp
+++ lab1/lab1/main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <fstream>
+#include <windows.h>
 #include <sys/stat.h>
 
 bool CheckSizeFile(std::string nameFile)
@@ -11,62 +12,61 @@ bool CheckSizeFile(std::string nameFile)
 	return (file.st_size > 0 && file.st_size < 2147483648);
 }
 
-void InputTextInFile(std::string nameFile, std::vector<std::string> &text) 
+
+void FindAndReplaceString(std::string & string, const std::string & searchStr, const std::string & replacementStr)
 {
-	std::ifstream inpFile(nameFile);
-	
+	std::string::size_type posStart;
+	std::string::size_type posEnd;
+
+	std::string localSTR = "";
+	posEnd = string.find(searchStr);
+
+	int lenSearchStr = searchStr.length();
+	int lenReplaceStr = replacementStr.length();
+
+	while (posEnd < string.length())
+	{
+		localSTR += string.substr(0, posEnd) + replacementStr;
+		posStart = posEnd + lenSearchStr;
+		string = string.substr(posStart);
+		posEnd = string.find(searchStr);
+	}
+	string = localSTR + string;
+}
+
+
+void Application(char * argv[])
+{
+	std::string inputFileName = argv[1];
+	std::string outputFileName = argv[2];
+	std::string replaceableStr = argv[3];
+	std::string replacementStr = argv[4];
+
+	std::ifstream inpFile(inputFileName);
 	if (!inpFile.is_open()) 
 	{
 		std::cout<< "Error open file\n";
 		system("pause");
 		exit(1);
 	}
-	else if (!CheckSizeFile(nameFile))
+	else if (!CheckSizeFile(inputFileName))
 	{
 		std::cout << "Error! File very big size or 0";
 		system("pause");
 		exit(1);
 	}
+
+	std::ofstream outFile(outputFileName);
+
 	while (!inpFile.eof())
 	{	
 		std::string string;
 		std::getline(inpFile, string);
-		text.push_back(string);
-	}
-}
-
-void OutputTextInFile(std::string nameFile, std::vector<std::string> text)
-{
-	std::ofstream outFile(nameFile);
-	for (auto i : text) {
-		outFile << i << "\n";
-	}
-}
-
-void FindAndReplaceString(std::vector<std::string> &lst_str, std::string replaceableStr, std::string replacementStr) 
-{
-	std::string::size_type pos;
-	for (int i = 0; i < int(lst_str.size()); i++)
-	{	
-		bool slice = false;
-		std::string localSTR = lst_str[i];
-		pos = localSTR.find(replaceableStr);
-		if (pos < localSTR.length())
+		if (replaceableStr.length() > 0)
 		{
-			lst_str[i].clear();
-			slice = true;
+			FindAndReplaceString(string, replaceableStr, replacementStr);
 		}
-		while (pos < localSTR.length())
-		{
-			localSTR.replace(pos, replaceableStr.length(), replacementStr);
-			lst_str[i] += localSTR.substr(0, pos + replacementStr.length());
-			localSTR.replace(0, pos + replacementStr.length(), "");
-			pos = localSTR.find(replacementStr);
-		}
-		if (slice && localSTR.size() > 0) 
-		{
-			lst_str[i] += localSTR.substr(0, pos + replacementStr.length());
-		}
+		outFile << string << "\n";
 	}
 }
 
@@ -89,22 +89,8 @@ int main(int argc, char * argv[])
 		PrintExample();
 	}
 	else
-	{
-		std::vector<std::string> lst_strings = {};
-		std::string nameInputFile = argv[1];
-		std::string nameOutputFile = argv[2];
-		std::string replaceableStr = argv[3];
-		std::string replacementStr = argv[4];
-		
-		InputTextInFile(nameInputFile, lst_strings);
-
-		if (replaceableStr.length() > 0)
-		{
-			FindAndReplaceString(lst_strings, replaceableStr, replacementStr);
-		}
-		OutputTextInFile(nameOutputFile, lst_strings);
+	{	
+		Application(argv);
 	}
-
-	system("pause");
 	return 0;
 }
---
+++ lab2/lab2/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::string nameF;
+	std::cout << "Input number \n";
+	std::cin >> nameF;
+	system("pause");
+	return 0;
+}
