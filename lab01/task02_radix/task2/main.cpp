90010a255908f1dccaaf2bca2c97f0c5d39dc2fb
AnatolyKorablyov <anatoly7755@gmail.com>
2016-02-14 14:01:00 +0300

    2 задание, исправил косяки при некорректном вводе

--- lab01/task02_radix/task2/main.cpp
+++ lab01/task02_radix/task2/main.cpp
@@ -2,7 +2,15 @@
 #include <string>
 #include <cmath>
 
-int SourceNotationToDec(std::string byte, const int & sourceNotation) 
+void CheckNumToSourceNotation(int num, int notation, bool & wasError) 
+{
+	if (num > notation - 1)
+	{
+		wasError = true;
+	}
+}
+
+int SourceNotationToDec(std::string byte, const int & sourceNotation, bool & wasError) 
 {
 	bool minus = false;
 	if ((int)byte[0] == 45)
@@ -16,22 +24,29 @@ int SourceNotationToDec(std::string byte, const int & sourceNotation)
 	{
 		if ((int)byte[0] < 91 && (int)byte[0] > 64)
 		{
+			CheckNumToSourceNotation((int)byte[0] - 55, sourceNotation, wasError);
 			result += ((int)byte[0] - 55) * pow(sourceNotation, count);
 		}
 		else if ((int)byte[0] > 47 && (int)byte[0] < 58)
 		{
+			CheckNumToSourceNotation((int)byte[0] - 48, sourceNotation, wasError);
 			result += ((int)byte[0] - 48) * pow(sourceNotation, count);
 		}
 		else
 		{
 			if ((int)byte[0] > 96 && (int)byte[0] < 123)
 			{
-				byte[0] = (char)((int)byte[0] - 31);
+				CheckNumToSourceNotation((int)byte[0] - 87, sourceNotation, wasError);
+				result += ((int)byte[0] - 87) * pow(sourceNotation, count);
 			}
 			else
 			{
-				std::cout << "incorrect format\n";
+				wasError = true;
 			}
+		}
+		if (wasError)
+		{
+			return 0;
 		}
 		byte = byte.substr(1);
 		count --;
@@ -43,20 +58,25 @@ int SourceNotationToDec(std::string byte, const int & sourceNotation)
 	return result;
 }
 
-std::string DecToDestinationNotation(int decNumber, const int & desNot)
+std::string DecToDestinationNotation(int decNumber, const int & destinationNot)
 {
 	bool minus = false;
+	std::string binary = "";
 	if (decNumber < 0)
 	{
 		minus = true;
 		decNumber *= -1;
 	}
-	std::string binary = "";
+	else if (decNumber == 0)
+	{
+		binary = "0";
+	}
+
 	while (decNumber > 0)
 	{
 		int num = 0;
-		num = decNumber - desNot * (decNumber / desNot);
-		decNumber = decNumber / desNot;
+		num = decNumber - destinationNot * (decNumber / destinationNot);
+		decNumber = decNumber / destinationNot;
 		if (num - 10 >= 0)
 		{
 			int sasd = num - 10;
@@ -67,7 +87,7 @@ std::string DecToDestinationNotation(int decNumber, const int & desNot)
 			binary = std::to_string(num) + binary;
 		}
 	}
-	if (minus)
+	if (minus && binary != "0")
 	{
 		binary = "-" + binary;
 	}
@@ -79,6 +99,7 @@ void PrintExample()
 	std::cout << "radix.exe <source notation> <destination notation> <value> \n";
 	std::cout << "EXAMPLE: radix.exe \"16\" \"10\" \"1F\"\n";
 }
+
 int main(int argc, char * argv[])
 {
 	if (argc > 4)
@@ -95,14 +116,31 @@ int main(int argc, char * argv[])
 	{
 		std::string value = argv[1];
 		int source = atoi(value.c_str());
+
 		value = argv[2];
 		int destination = atoi(value.c_str());
+
 		value = argv[3];
+		bool wasError = false;
+		int decNumber = SourceNotationToDec(value, source, wasError);
 
-		int decNumber = SourceNotationToDec(value, source);
-		std::string result = DecToDestinationNotation(decNumber, destination);
-
-		std::cout << result << "\n";
+		if ((source >= 2 && source <= 36) && (destination >= 2 && destination <= 36))
+		{
+			if (!wasError)
+			{
+				std::string result = DecToDestinationNotation(decNumber, destination);
+				std::cout << result << "\n";
+			}
+			else
+			{
+				std::cout << "incorrect value. value = 0-9 and A-Z and should be in the source notation\n";
+				std::cout<<"Example: task2.exe \"36\" \"10\" \"27WZ\"\n";
+			}
+		}
+		else
+		{
+			std::cout << "incorrect input bit. Bit should be from 2 to 36 \n";
+		}
 	}
 	system("pause");
 	return 0;
